7e7d7fadd61381e49190b2d5230f3d1328d156bb
wongpeeng <635711867@qq.com>
2016-05-10 18:46:15 +0800

    rm

--- rda/rda/items.py
+++ rda/rda/items.py
@@ -5,14 +5,10 @@
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
 
-from scrapy.item import Item, Field 
+import scrapy
 
 
-class TutorialItem(Item):
+class RdaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
-class DmozItem(Item):
-    title=Field()
-    link=Field()
-    desc=Field()
